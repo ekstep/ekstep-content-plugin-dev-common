3209753ab6d78a6c76e01d98837d4692f5730dcb
Deepak N <endeep123@gmail.com>
2017-05-18 16:41:43 +0530

    Issue ekstep/Contributed-Plugins#139 feat: Use execSync as spawnSync doesn' run synchronous on windows

--- bin/updateFrameworkDependencies.js
+++ bin/updateFrameworkDependencies.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
 var rimraf = require('rimraf');
-var spawnSync = require('child_process').spawnSync;
-
+var execSync = require('child_process').execSync;
 
 rimraf.sync('bower_components/content-editor*');
 rimraf.sync('bower_components/renderer*');
 rimraf.sync('node_modules/eslint-config-ekstep-content-plugin');
-spawnSync('node_modules/bower/bin/bower', ['cache', 'clean'], { stdio: 'inherit' });
-spawnSync('node_modules/bower/bin/bower', ['install'], { stdio: 'inherit' });
-spawnSync('npm', ['install'], { stdio: 'inherit' });
+execSync('node_modules/bower/bin/bower cache clean', { stdio: 'inherit' });
+execSync('node_modules/bower/bin/bower install', { stdio: 'inherit' });
+execSync('npm install', { stdio: 'inherit' });
