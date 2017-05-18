f8471d0982651e7dfd6336bf05ce1d157a8e8511
Deepak N <endeep123@gmail.com>
2017-05-18 16:47:25 +0530

    Issue ekstep/Contributed-Plugins#139 feat: Fix running bower on windows

--- bin/updateFrameworkDependencies.js
+++ bin/updateFrameworkDependencies.js
@@ -6,6 +6,6 @@ var execSync = require('child_process').execSync;
 rimraf.sync('bower_components/content-editor*');
 rimraf.sync('bower_components/renderer*');
 rimraf.sync('node_modules/eslint-config-ekstep-content-plugin');
-execSync('node_modules/bower/bin/bower cache clean', { stdio: 'inherit' });
-execSync('node_modules/bower/bin/bower install', { stdio: 'inherit' });
+execSync('node node_modules/bower/bin/bower cache clean', { stdio: 'inherit' });
+execSync('node node_modules/bower/bin/bower install', { stdio: 'inherit' });
 execSync('npm install', { stdio: 'inherit' });
