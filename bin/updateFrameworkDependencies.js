12c2e75043876aa4f2639cea7894af1333ed3407
unknown <akshaya.rathinavel@tarento.com>
2018-10-12 14:48:30 +0530

    Issue #9 fix: content framework dependency changed

--- bin/updateFrameworkDependencies.js
+++ bin/updateFrameworkDependencies.js
@@ -4,8 +4,8 @@ var rimraf = require('rimraf');
 var execSync = require('child_process').exec;
 
 rimraf.sync('bower_components/content-editor*');
-rimraf.sync('bower_components/renderer*');
-rimraf.sync('node_modules/eslint-config-ekstep-content-plugin');
+rimraf.sync('bower_components/content-player*');
+
 execSync('node node_modules/bower/bin/bower cache clean', { stdio: 'inherit' });
 execSync('node node_modules/bower/bin/bower install', { stdio: 'inherit' });
 execSync('npm install', { stdio: 'inherit' });
